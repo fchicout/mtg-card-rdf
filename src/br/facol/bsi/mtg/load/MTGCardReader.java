f514130210ea5de11e20af038a48d01652c1a797
fchicout <fchicout@localhost.localdomain>
2016-05-25 01:32:42 -0300

    removing old code

--- src/br/facol/bsi/mtg/load/MTGCardReader.java
+++
@@ -1,8 +0,0 @@
-package br.facol.bsi.mtg.load;
-
-import java.io.File;
-
-public class MTGCardReader {
-	private File bruteJsonData = new File("/home/fchicout/Documents/nt_AllSets-x-unix.json");
-	
-}
